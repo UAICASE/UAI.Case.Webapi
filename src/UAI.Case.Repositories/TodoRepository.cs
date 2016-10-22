a204a4a20867db591eed93cd3c59ad1674f5ce93
Nicolás Battaglia <nicolas.battaglia@uai.edu.ar>
2016-10-22 20:54:16 -0300

    Repositories + exceptions + utilites + security + appservice

---
+++ src/UAI.Case.Repositories/TodoRepository.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.AspNetCore.Http;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UAI.Case.Domain.Academico;
+using UAI.Case.Domain.Common;
+using UAI.Case.InMemoryProvider;
+
+namespace UAI.Case.Repositories
+{
+    public interface ITodoRepository : IRepository<Todo>
+    {
+         long  GetPendientes(Guid IdUsuario);
+    }
+    public class TodoRepository : Repository<Todo>, ITodoRepository
+    {
+        public TodoRepository(IDbContext<Todo> db, IHttpContextAccessor context) : base(context,db) { }
+
+        public long  GetPendientes(Guid IdUsuario)
+        {
+
+            try
+            {
+                String qry = "SELECT COUNT(ag.id) FROM AlumnoCursoGrupo ag, GrupoTodo gt "
+            + "WHERE gt.Grupo.Id=ag.Grupo.Id AND ag.Alumno.Id = @p0 AND gt.Done = @p1";
+
+
+
+                //String strQry = "FROM GrupoTodo as todo WHERE todo.Done='false' AND todo.Grupo.Id  IN (SELECT Grupo.Id From AlumnoCursoGrupo ac WHERE ac.Alumno.Id = :id)";
+                //IQuery qry = Session.CreateQuery(qry2);
+
+                //qry.SetParameter("id", IdUsuario);
+                //qry.SetParameter("done", false);
+
+
+                long res = _db.GetAll().Count();// _db.FromSQL<AlumnoCursoGrupo>(qry, IdUsuario.ToString(), "False").ToList().Count();
+                return res;
+            }
+            catch (Exception)
+            {
+
+                return -1;
+            }
+
+            
+        }
+    }
+}
---
+++ src/UAI.Case.Utilities/Mailer.cs
@@ -0,0 +1,105 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UAI.Case.Domain.Common;
+
+namespace UAI.Case.Utilities
+{
+    public static class Mailer
+    {
+
+
+        private async static Task Send(MimeMessage message)
+        {
+            //sacar a config
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587);
+            client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+            // Note: only needed if the SMTP server requires authentication
+            client.Authenticate("uai.case.registros@gmail.com", "3l3c720n");
+
+            await client.SendAsync(message);
+            client.Disconnect(true);
+
+
+        }
+
+        public async static Task SendActivateRequest(Usuario usuario, string currentUrl)
+        {
+            try
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("UAI CASE - Registros", "uai.case.registros@gmail.com"));
+                message.To.Add(new MailboxAddress(String.Format("{0} {1}",usuario.Nombre,usuario.Apellido), usuario.Mail));
+                message.Subject = "UAI CASE -Activar cuenta";
+
+                var builder = new BodyBuilder();
+                builder.HtmlBody = String.Format(@"Su usuario es: {0}<br/>
+                        Su passwod temporal es: {1}<br/>
+                        haga click <a href='http://{2}?id={3}&activate={4}'>aquí</a> para activar su cuenta ",
+                        usuario.Mail, usuario.Password, currentUrl, usuario.Id, UAI.Case.Security.Cryptography.MD5Hash(usuario.RegisterToken));
+
+                message.Body = builder.ToMessageBody();
+
+                await Send(message);
+                
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                throw new Exception(e.Message,e);
+            }
+        }
+
+
+        public async static Task SendRegistrationRequest(string currentUrl, string registerToken, string mail)
+        {
+            ////TODO: Factorizar
+            //try
+            //{
+            //    SmtpClient client = new SmtpClient();
+            //    client.Port = 587;
+            //    client.Host = "smtp.gmail.com";
+            //    client.EnableSsl = true;
+            //    client.Timeout = 10000;
+            //    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //    client.UseDefaultCredentials = false;
+            //    client.Credentials = new System.Net.NetworkCredential("uai.case.registros@gmail.com", "3l3c720n");
+
+
+
+            //    MailMessage mm = new MailMessage(
+            //        "uai.case.registros@gmail.com", mail, "UAI CASE - Registrarse",
+
+            //        " haga click <a href='http://" + currentUrl + "?register=" + registerToken + "'>aquí</a> para registrarse");
+
+
+            //    mm.BodyEncoding = UTF8Encoding.UTF8;
+            //    mm.IsBodyHtml = true;
+            //    mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            //    client.Send(mm);
+            //    return true;
+
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.Write(e.Message);
+            //    throw new Exception(e.Message, e);
+            //}
+
+            await Send(null);
+        }
+
+       
+    }
+}
