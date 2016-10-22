558b6d3bef60f60bd496628a3c7fa0dfe2f2dd67
Nicolás Battaglia <nicolas.battaglia@uai.edu.ar>
2016-10-22 16:47:09 -0300

    bluemix config

--- src/UAI.Case.Webapi/Startup.cs
+++ src/UAI.Case.Webapi/Startup.cs
@@ -39,5 +39,26 @@ namespace UAI.Case.Webapi
 
             app.UseMvc();
         }
+
+
+
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseConfiguration(config)
+                        .UseStartup<Startup>()
+                        .Build();
+
+            host.Run();
+        }
     }
+
+
+
+
 }
