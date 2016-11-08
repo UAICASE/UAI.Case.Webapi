2d02176c9478dfd91e58c09f6d7d747bb4e234c3
Nicolás Battaglia <nicolas.battaglia@uai.edu.ar>
2016-11-08 15:47:40 -0300

    No RSA

--- src/UAI.Case.Security/Keys.cs
+++ src/UAI.Case.Security/Keys.cs
@@ -9,45 +9,45 @@ using System.Threading.Tasks;
 
 namespace UAI.Case.Security
 {
-    public static class Keys
-    {
-        private static RsaSecurityKey _rsaKey = null;
-        private static RSAParameters _rsaParams;
+    //public static class Keys
+    //{
+    //    private static RsaSecurityKey _rsaKey = null;
+    //    private static RSAParameters _rsaParams;
         
         
         
 
-        public static RsaSecurityKey RSAKey
-        {
-            get
-            {
-                if (_rsaKey == null)
-                {
-                    _rsaParams = GetRandomKey();
-                    _rsaKey = new RsaSecurityKey(_rsaParams);
+    //    //public static RsaSecurityKey RSAKey
+    //    //{
+    //    //    get
+    //    //    {
+    //    //        if (_rsaKey == null)
+    //    //        {
+    //    //            _rsaParams = GetRandomKey();
+    //    //            _rsaKey = new RsaSecurityKey(_rsaParams);
 
                     
-                }
+    //    //        }
 
-                return _rsaKey;
-            }
-        }
+    //    //        return _rsaKey;
+    //    //    }
+    //    //}
 
 
-        private static RSAParameters GetRandomKey()
-        {
-            using (var rsa = new RSACryptoServiceProvider(2048))
-            {
-                try
-                {
-                    return rsa.ExportParameters(true);
-                }
-                finally
-                {
-                    rsa.PersistKeyInCsp = false;
-                }
-            }
-        }
+    //    //private static RSAParameters GetRandomKey()
+    //    //{
+    //    //    using (var rsa = new RSACryptoServiceProvider(2048))
+    //    //    {
+    //    //        try
+    //    //        {
+    //    //            return rsa.ExportParameters(true);
+    //    //        }
+    //    //        finally
+    //    //        {
+    //    //            rsa.PersistKeyInCsp = false;
+    //    //        }
+    //    //    }
+    //    //}
 
-    }
+    //}
 }
